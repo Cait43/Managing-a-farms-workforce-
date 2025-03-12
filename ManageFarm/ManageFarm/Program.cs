683d9c1eae3401eb562d63d895701f44401d1863
Maxine770 <145765923+Maxine770@users.noreply.github.com>
2025-03-12 23:02:36 +0000

    WIP on interfacing with website

--- ManageFarm/ManageFarm/Program.cs
+++ ManageFarm/ManageFarm/Program.cs
@@ -1,7 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using ManageFarm.Data.ManageFarmContext;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddDbContext<cs>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("cs") ?? throw new InvalidOperationException("Connection string 'cs' not found.")));
 
 var app = builder.Build();
 
